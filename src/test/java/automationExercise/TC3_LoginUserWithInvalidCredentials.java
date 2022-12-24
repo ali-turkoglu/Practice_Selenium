d1aae16853068e32ef7ce5b161e69233f29e6087
Ali Turkoglu <97362442+ghosTRaveler06@users.noreply.github.com>
2022-12-24 17:44:04 +0300

    TC3 is finished successfully

--- src/test/java/automationExercise/TC3_LoginUserWithInvalidCredentials.java
+++ src/test/java/automationExercise/TC3_LoginUserWithInvalidCredentials.java
@@ -25,18 +25,14 @@ public class TC3_LoginUserWithInvalidCredentials {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
-
 //2. Navigate to url 'http://automationexercise.com'
         driver.get("http://automationexercise.com");
-
 
 //3. Verify that home page is visible successfully
         String expectedTitle="Automation Exercise";
         String actualTitle= driver.getTitle();
         //System.out.println("actualTitle = " + actualTitle);
-
         Assertions.assertEquals(expectedTitle,actualTitle);
-
 
 //4. Click on 'Signup / Login' button
         WebElement signupButton=driver.findElement(By.xpath("//a[text()=' Signup / Login']"));
@@ -44,12 +40,33 @@ public class TC3_LoginUserWithInvalidCredentials {
 
 
 //5. Verify 'Login to your account' is visible
+        String expectedLoginFormHeader="Login to your account";
+        String actualLoginFormHeader=driver.findElement(By.xpath("//h2[.='Login to your account']")).getText();
+        //System.out.println("actualLoginFormHeader = " + actualLoginFormHeader);
+        Assertions.assertEquals(expectedLoginFormHeader,actualLoginFormHeader);
 
 
+//6. Enter incorrect email address and password
+        String invalidEmail="johnDoe@test.com";
+        String invalidPassword="12345";
+        WebElement emailInputBox= driver.findElement(By.xpath("//input[@data-qa='login-email']"));
+        WebElement passwordInputBox= driver.findElement(By.xpath("//input[@data-qa='login-password']"));
+        emailInputBox.sendKeys(invalidEmail);
+        passwordInputBox.sendKeys(invalidPassword);
 
-//        6. Enter incorrect email address and password
-//        7. Click 'login' button
-//        8. Verify error 'Your email or password is incorrect!' is visible
+//7. Click 'login' button
+        WebElement loginButton= driver.findElement(By.xpath("//button[@data-qa='login-button']"));
+        loginButton.click();
+
+//8. Verify error 'Your email or password is incorrect!' is visible
+        String expectedErrorMessage="Your email or password is incorrect!";
+        String actualErrorMessage=driver.findElement(By.xpath("//p[.='Your email or password is incorrect!']")).getText();
+        //System.out.println("actualErrorMessage = " + actualErrorMessage);
+        Assertions.assertEquals(expectedErrorMessage,actualErrorMessage);
+        //OR
+        WebElement errorMessage=driver.findElement(By.xpath("//p[.='Your email or password is incorrect!']"));
+        Assertions.assertTrue(errorMessage.isDisplayed());
+
 
 
     }
